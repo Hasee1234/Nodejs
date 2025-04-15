a6f8714d2722f24e1f5cd87e3d8e1dc0012a3333
hasee <haseemirza123@gmail.com>
2025-04-15 21:17:58 +0500

    working on encryption of password

--- server.js
+++ server.js
@@ -237,6 +237,8 @@
 //   })
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const Todo = require("./Models/Todo"); // Import the Todo model
@@ -442,22 +444,6 @@ start();
 //how to do same work in nextjs
 
 //authentication you take data in login signup sp post method
-app.post("/auth/login",(req, res) => {
-  try {
-    console.log('req.body recieved',req.body);
-    res.json({
-      data: req.body,
-      status: "success"
-    }); 
-    
-  } catch (error) {
-   res.json({
-      data:[],
-      status:"error",
-      error:error
-   }) 
-  }
-});
 
 
 const userSchema=new mongoose.Schema({
@@ -480,6 +466,58 @@ const userSchema=new mongoose.Schema({
 })
 const User=mongoose.model('User',userSchema)
 
+app.post("/auth/login",async(req, res) => {
+  try {
+  if(!req.body?.email){//to say user to enter email and password
+    res.json({
+      data:[], 
+      status:"error",
+      error:"email is required"
+    })}
+  if(!req.body?.password){
+    res.json({
+      data:[],
+      status:"error",
+      error:"password is required"
+    })}
+    const userFound=await User.findOne({email:req.body?.email})//to find the user in db
+    if(!userFound){//if user not found
+      res.json({
+        data:[],
+        status:"error",
+        error:"user not found"
+      })
+    }
+    console.log("user found",userFound);
+    
+    var passwordIsValid=bcrypt.compareSync(
+      req.body.password,
+      userFound.password
+    )//to compare the password in db and entered password
+    if(!passwordIsValid){//if password is not valid
+      res.json({
+        data:[],
+        status:"error",
+        error:"password is not valid"
+      })
+    }
+    res.json({
+      data: {
+        token:"asdf",
+        email:userFound.email,
+        name:userFound.name,
+        address:userFound.address
+      },
+      status: "success"
+    }); 
+     
+  } catch (error) {
+   res.json({
+      data:[],
+      status:"success",
+   }) 
+  }
+});
 
 app.post("/auth/signup",async(req, res) => {
   try {
@@ -491,16 +529,24 @@ app.post("/auth/signup",async(req, res) => {
         error:"password is required"
       })
     }
-    let newUser= await new User({
+    var hash=bcrypt.hashSync(req.body.password,8);//to encypt passwords
+    console.log('hash',hash);
+    
+    let newUser= new User({
       name:req.body?.name,
       email:req.body?.email,
-      password:req.body?.password,
+      password:hash,  
       address:req.body?.address
     })
     let output =newUser.save()
 
     res.json({
-      data: req.body,
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+        password: hash, // showing the hashed password in postman
+        address: req.body.address
+      },
       status: "success"
     }); 
     
